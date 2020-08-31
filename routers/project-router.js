ab59a7fe4c5022beed37eb10ea813dad3f34ecd4
Simone Roy <simonecarolynr@gmail.com>
2020-08-30 19:05:13 -0600

    routers layed out

--- routers/project-router.js
+++ routers/project-router.js
@@ -6,27 +6,36 @@ const taskDb = require('../models/task-model')
 const project = express.Router()
 
 //CREATES new project /project
+project.post('/project', async (req, res, next) => {
 
+})
+
+//CREATES new task /project/:id/task
+project.post('/project/:id/task', async (req, res, next) => {
+
+})
 
 //RETRIEVES all projects /project
+project.get('/project', async (req, res, next) => {
+    
+})
+
+// *** STRETCH: RETRIEVES project by ID /project/:id ***
 
 
-//RETRIEVES project by ID /project/:id
+// *** STRETCH: RETRIEVES resources from a project /project/:id/resources ***
 
 
-//RETRIEVES resources from a project /project/:id/resources
+// *** STRETCH???: RETRIEVES all tasks from a project /project/:id/task ***
 
 
-//RETRIEVES all tasks from a project /project/:id/task
+//*** STRETCH: RETRIEVES a task by it's ID from a project /project/:projectId/task/:taskId ***
 
 
-//RETRIEVES a task by it's ID from a project /project/:projectId/task/:taskId
+//*** STRETCH: UPDATES project by ID /project/:id ***
 
 
-//UPDATES project by ID /project/:id
-
-
-//DELETES project /project/:id
+//*** STRETCH: DELETES project /project/:id ***
 
 
 
--- routers/resources-router.js
+++ routers/resources-router.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const db = require('../models/resource-model')
 
-const resources = express.Router()
+const resource = express.Router()
 
 //CREATES a resource /resource
+resource.post('/resource', async (req, res, next) => {
 
+})
 
 //RETRIEVES all resources /resource
+resource.get('resource', async (req, res, next) => {
+    
+})
+
+// *** STRETCH: RETRIEVES resource by ID /resource/:id ***
 
 
-//RETRIEVES resource by ID /resource/:id
+// *** STRETCH: UPDATES resource by ID /resource/:id ***
 
 
-//UPDATES resource by ID /resource/:id
-
-
-//DELETES resource by ID /resource/:id
+// *** STRETCH: DELETES resource by ID /resource/:id ***
 
 
 
-module.exports = resources
+module.exports = resource
